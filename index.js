616091a4d81baf2b6a6f5f83e7ae531e4568c7df
Bruna Aguiar <baguiar861@gmail.com>
2023-10-23 09:12:36 -0300

    Projeto Finalizado!

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("ola")
+let nome = "Joao"
+let XP = 6030
+let nivel = ""
+if(XP < 1000) {
+    nivel = "Ferro"
+} else if(XP > 1000 && XP <= 2000) {
+    nivel = "Bronze"
+} else if(XP > 2000 && XP <= 5000) {
+    nivel = "Prata"
+} else if(XP > 6000 && XP <= 7000) {
+    nivel = "Ouro"
+} else if(XP > 7000 && XP <= 8000) {
+    nivel = "Platina"
+} else if(XP > 8000 && XP <= 9000) {
+    nivel = "Ascendente"
+} else if(XP > 9000 && XP <= 10000) {
+    nivel = "Imortal"
+} else if(XP >= 10001) {
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
